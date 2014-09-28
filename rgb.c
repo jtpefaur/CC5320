c6fe117818459b0f8d788b8c8c665d4475fec4f1
Jose Pefaur <jose.pefaur@gmail.com>
2014-09-28 18:18:47 -0300

    Variante

--- rgb.c
+++ rgb.c
@@ -11,34 +11,24 @@ typedef struct {
 #define ROWS 5000
 #define COLUMNS 20000
 
-Tpixel buffer[ROWS][COLUMNS];
+Tpixel buffer[ROWS*COLUMNS];
 
 void rgb_verd()
 {
-	unsigned int i,j;
-	for(i=0;i<ROWS-1;i++)
+	unsigned int i;
+	for(i=0;i<(ROWS*COLUMNS)-2;i+=2)
 	{
-		for(j=0;j<COLUMNS;j+=2)
-		{
-			*((long *)&buffer[i][j]) = 0x0000000100000100;
-		}
-
+		*((long *)&buffer[i]) = 0x00000100000100;
 	}
 
-	for(j=0;j<COLUMNS-2;j+=2)
-	{
-		*((long *)&buffer[ROWS-1][j]) = 0x0000000100000100;
-	}
-
-	buffer[ROWS-1][j].r=0;
-	buffer[ROWS-1][j].g=1;
-	buffer[ROWS-1][j].b=0;
-	
-	buffer[ROWS-1][j+1].r=0;
-	buffer[ROWS-1][j+1].g=1;
-	buffer[ROWS-1][j+1].b=0;
+	buffer[i].r=0;
+	buffer[i].g=1;
+	buffer[i].b=0;
 
 
+	buffer[i+1].r=0;
+	buffer[i+1].g=1;
+	buffer[i+1].b=0;
 }
 
 
