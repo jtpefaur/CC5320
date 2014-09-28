261a8aa79ec45c6daf1574186c1202e10ef8b476
Jose Pefaur <jose.pefaur@gmail.com>
2014-09-28 16:53:27 -0300

    Just the last 6 bytes are not casted

--- rgb.c
+++ rgb.c
@@ -16,22 +16,31 @@ Tpixel buffer[ROWS][COLUMNS];
 void rgb_verd()
 {
 	unsigned int i,j;
-	for(i=0;i<ROWS;i++)
+	for(i=0;i<ROWS-1;i++)
 	{
-		for(j=0;j<COLUMNS-2;j+=2)
+		for(j=0;j<COLUMNS;j+=2)
 		{
 			*((long *)&buffer[i][j]) = 0x0000000100000100;
 		}
 
-		buffer[i][j].r=0;
-		buffer[i][j].g=1;
-		buffer[i][j].b=0;
-
-		buffer[i][j+1].r=0;
-		buffer[i][j+1].g=1;
-		buffer[i][j+1].b=0;
 	}
+
+	for(j=0;j<COLUMNS-2;j+=2)
+	{
+		*((long *)&buffer[ROWS-1][j]) = 0x0000000100000100;
+	}
+
+	buffer[ROWS-1][j].r=0;
+	buffer[ROWS-1][j].g=1;
+	buffer[ROWS-1][j].b=0;
+	
+	buffer[ROWS-1][j+1].r=0;
+	buffer[ROWS-1][j+1].g=1;
+	buffer[ROWS-1][j+1].b=0;
+
+
 }
+
 
 int main(int argc,char* argv[])
 {
