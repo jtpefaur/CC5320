83a8413dab3712de7546068a5f979144ccd5117a
Jose Pefaur <jose.pefaur@gmail.com>
2014-09-28 10:19:53 -0300

    Recorrer por columnas

--- rgb.c
+++ rgb.c
@@ -16,10 +16,11 @@ Tpixel buffer[ROWS][COLUMNS];
 void rgb_verd()
 {
 	unsigned int i,j;
-	for(j=0;j<COLUMNS;j++)
+	for(i=0;i<ROWS;i++)
 	{
-		for(i=0;i<ROWS;i++)
+		for(j=0;j<COLUMNS;j++)
 		{
+		
 			buffer[i][j].r=0;
 			buffer[i][j].g=1;
 			buffer[i][j].b=0;
